08dc006bb5c63aca2d7b63f7530648afa65fcbfe
kikmak42 <kikmak42@null.net>
2013-12-20 15:18:12 +0530

    Project Initiated, Net beans support added.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ JARvis
 ======
 
 The JAVA voice controller for your Computer, designed using Google's Speech-to-Text Engine
+
+The Project has be initiated on 20th December.
+Excepted complementation date 26th January.
+
+Tasks:
+1. Port the js library of Web Speech API to JAVA.
+2. Use JAVA for controlling the system (currently windows)
+
+
---
+++ src/jarvis/JARvis.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarvis;
+
+/**
+ *
+ * @author kaushik
+ */
+public class JARvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jarvis");
+        // TODO code application logic here
+    }
+}
